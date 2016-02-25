3c5cfcbb5e9e2cd0947864606751975e89489438
Max <max.mesirow@gmail.com>
2016-02-25 18:57:24 -0500

    First Commit

---
+++ backend.py
@@ -0,0 +1,12 @@
+IL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Host for sending e-mail.
+EMAIL_HOST = 'localhost'
+
+# Port for sending e-mail.
+EMAIL_PORT = 1025
+
+# Optional SMTP authentication information for EMAIL_HOST.
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
